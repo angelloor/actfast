ae8306ad8088232e094032a4f2b2304c534941bc
Angel Miguel Loor Manzano <miguelangelloor96@gmail.com>
2021-01-07 19:05:54 -0500

    Puliendo Acta Digital

--- README.md
+++ README.md
@@ -3,8 +3,15 @@ Sistema para control de inventario y generación de actas CNE
 
 Faltante del sistema Actfast
 •Validar creación de usuarios (que no se repitan y que solo permita un administrador).
-•Terminar modulo credenciales de sistemas.
 •Validación de vista según su rol de usuario (También controlar el acceso de rutas).
+
+ordenas los datos que carga en los select de A - Z
+
+
+•Terminar modulo credenciales de sistemas.
+
+Validacion que no se repitan el codigo del activo, el numero de cedula del funcionario
+
 •Terminar Modulo de reportes.
 •Hacer POPUP en los botones de acceder de reportes y actas.
 •Terminar Generación de Actas.
--- modelo/actaDigital.php
+++ modelo/actaDigital.php
@@ -1,5 +1,5 @@
 <?php
-    require('../fpdf/fpdf.php');
+    require('../fpdf/wrapper.php');
     require 'conexion.php';
 
     $totalSistemas = $_GET['totalSistemas'];
@@ -48,14 +48,14 @@
 
     $totalSistemas = $_GET['totalSistemas'];
 
-    class PDF extends FPDF
+    class PDF extends Wrapper
     {
         function Header()
         {
             $this->Image('../assets/img/img_acta.png',25,10,40);
             $this->SetFont('Times','B',11);
             $this->Ln(10);
-            $this->Cell(60);
+            $this->Cell(45);
             $this->SetTextColor(31,78,121);
             $this->MultiCell(115,5,utf8_decode('UNIDAD PROVINCIAL DE SEGURIDAD INFORMÁTICA Y PROYECTOS TECNOLÓGICOS ELECTORALES DE PASTAZA'));
             $this->Ln(5);
@@ -73,42 +73,67 @@
             $this->SetTextColor(42,81,147);
             $this->SetFont('Times','',8);
             $this->SetRightMargin(33);
-            $this->MultiCell(0,3,utf8_decode("Puyo / Ecuador \n www.cnedppastaza.gob.ec \n Av. Alberto Zambrano \n Palacios s/n \n PBX: (593)2 885 145/885 359 \nFO-01(DG-SM-AD-09)"),0,'R');
+            $this->MultiCell(0,3,utf8_decode("Puyo / Ecuador \n www.cnedppastaza.gob.ec \n Av. Alberto Zambrano \n Palacios s/n \n PBX: (593)2 885 145/885 359"),0,'R');
             $this->Image('../assets/img/ec.png',180,265,2);
+            $this->SetFont('Times','',10);
+            $this->SetTextColor(0,0,0);
+            $this->Cell(165,10,utf8_decode('Página ').$this->PageNo().' / {nb}',0,0,'C');
+            $this->Cell(-120,10,'ISO - FO-01(DG-SM-AD-09)',0,0,'R');
 
         }
-
+        
         function parrafo($texto)
         {
             $txt = $texto;
-            $this->SetFont('Times','',10);
-            $this->SetRightMargin(25);
-            $this->SetLeftMargin(25);    
+            $this->SetFont('Times','',10);  
             $this->MultiCell(0,5,utf8_decode($txt)  );
-            $this->Ln();
             $this->SetFont('','I');
         }
     }
     
     $pdf = new PDF();
+    $pdf->SetRightMargin(25);
+    $pdf->SetLeftMargin(25);  
     $pdf->AliasNbPages();
+    $pdf->SetAutoPageBreak(true,35);
     $pdf->AddPage();
     $pdf->SetTextColor(31,78,121);
     $pdf->SetFont('Times','B',12);
+    $pdf->Ln();
     $pdf->Cell(0,10,utf8_decode('ACTA ENTREGA RECEPCION DE CREDENCIALES DIGITALES'),0,1,'C');
     $pdf->SetTextColor(0,0,0);
     $pdf->parrafo("La presente acta de entrega recepción tiene por objeto otorgar credenciales para el manejo de los siguientes sistemas: ");
+    $pdf->Ln();
+
+    // Cabecera de la tabla
+    $pdf->SetFont('Times','B',10);
+    $pdf->SetX(25);
+    $pdf->Cell(40,6, "SISTEMA", 1,0,'C',0);
+    $pdf->Cell(40,6, "URL", 1,0,'C',0);
+    $pdf->Cell(40,6, "USUARIO", 1,0,'C',0);
+    $pdf->Cell(40,6, "CLAVE", 1,1,'C',0);
+    //definiar distancias de cada celda
+    $pdf->SetWidths(Array(40,40,40,40));
+    $pdf->SetLineHeight(5);
+    $pdf->SetFont('Times','',8);
+
     for ($i=1; $i <=$totalSistemas ; $i++) {
         $sistema = $_GET['sistema'.$i];
         $url = $_GET['url'.$i];
         $usuario = $_GET['usuario'.$i];
         $clave = $_GET['clave'.$i];
-        $sistemas = $sistemas.$i.".- ".$sistema." - URL = ".$url." - USUARIO = ".$usuario." - CLAVE = ".$clave."\n";
+        $pdf->Row(25, 0,Array(
+            $sistema,
+            $url,
+            $usuario,
+            $clave,
+        ), 'C');
     }
-    $pdf->parrafo($sistemas);
+    $pdf->Ln();
     $pdf->parrafo("a $nombreFuncionario con numero de cedula $cedula, cuyo cargo es $nombreCargo para el proceso Electoral $periodo, El funcionario receptor de las credenciales está obligado al complimiento de:");
+    $pdf->Ln();
     $pdf->parrafo("1. Las credenciales entregadas al funcionario para el manejo de los sistemas antes mencionados son para uso institucional e intransferible, y su utilización es de exclusiva responsabilidad del funcionario.\n2. El funcionario $nombreFuncionario, se compromete a la no divulgación y buen uso de la información facilitada por la institución con total confidencialidad, de incumplir con este compromiso será responsable de las consecuencias establecida en el artículo 190.- 'Apropiación fraudulenta por medios electrónicos' del COIP.\n3. En caso de pérdida, olvido o sustracción del usuario y/o clave de acceso para el manejo de los sistemas, el funcionario deberá comunicar al área de tecnología del Consejo Nacional Electoral Delegación Pastaza, de manera inmediata. \n4. Las credenciales de acceso serán entregadas de manera persona al funcionario responsable de la misma.");
-
+    $pdf->Ln();
     $pdf->parrafo("Para la constancia de la actuado y en fe de conformidad y aceptación, se suscribe la presente acta en dos originales de igual valor y efecto para las personas que intervienen en esta diligencia, en la ciudad de $ciudad, a los $dia días del mes de $mes del $año.");
     $pdf->SetTextColor(0,0,0);
     //Firmas de las actas
@@ -125,6 +150,20 @@
     $pdf->ln(10);
     $pdf->MultiCell(0,5,utf8_decode("$nombreFuncionario \n$nombreCargo"),0,'C');
 
-    $pdf->Output();
+    $nombrePdf = "actaDigital".$nombreFuncionario;
+    $pdf->Output('',nombrePdf($nombrePdf),true);
+
+    function nombrePdf($nombre){
+        $fechaTotal = getdate();
+        if($fechaTotal['wday'] <= 9){
+            $dia = "0".$fechaTotal['wday'];
+        }
+        if($fechaTotal['mon'] <= 9){
+            $mes = "0".$fechaTotal['mon'];
+        }
+        $fechaFinal = $fechaTotal['year']."-".$mes."-".$dia;
+        $nombreFinal = $nombre.$fechaFinal.".pdf";
+        return $nombreFinal;
+    }
 
 ?>
