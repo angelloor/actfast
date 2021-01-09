fdff2ecfbe79a7e5d22b14cee05e8128628fa32d
Angel Miguel Loor Manzano <miguelangelloor96@gmail.com>
2021-01-09 18:56:56 -0500

    Cambiando Modelo Login

--- modelo/login.modelo.php
+++ modelo/login.modelo.php
@@ -7,16 +7,26 @@
         public function consultarUsuario($usuario, $clave){
 
             $conexion =  new Conexion();
-            $stmt = $conexion->prepare("select u.id_usuario, p.nombre_persona, u.nombre_usuario, u.clave, ru.nombre_rol_usuario from usuario u inner join persona p on u.persona_id = p.id_persona inner join rol_usuario ru on u.rol_usuario_id = ru.id_rol_usuario where nombre_usuario = :usuario");
-            $stmt->bindValue(":usuario",$usuario,PDO::PARAM_STR); 
+
+            $stmt = $conexion->prepare("SELECT COUNT(*) FROM usuario WHERE NOMBRE_USUARIO = :usuario");
+            $stmt->bindValue(":usuario",$usuario,PDO::PARAM_STR);
             $stmt->execute();
-            $datos = $stmt->fetch(PDO::FETCH_ASSOC);
-            
-            if($datos['clave'] == $clave){
-                $_SESSION['user'] = $datos['nombre_usuario'];
-                return "OK";
+            $usuarioExiste = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if($usuarioExiste['COUNT(*)'] == 0 ){
+                return "El usuario no existe";
             }else{
-                return "Usuario o contraseña incorrectos";
+                $stmt = $conexion->prepare("SELECT u.id_usuario, p.nombre_persona, u.nombre_usuario, u.clave, ru.nombre_rol_usuario FROM usuario u INNER JOIN persona p ON u.persona_id = p.id_persona INNER JOIN rol_usuario ru ON u.rol_usuario_id = ru.id_rol_usuario WHERE nombre_usuario = :usuario");
+                $stmt->bindValue(":usuario",$usuario,PDO::PARAM_STR); 
+                $stmt->execute();
+                $datos = $stmt->fetch(PDO::FETCH_ASSOC);
+                if($datos['clave'] == $clave){
+                    $_SESSION['user'] = $datos['nombre_usuario'];
+                    return "OK";
+                }else{
+                    return "Usuario o contraseña incorrectos".$datos['clave']." - ".$clave;
+                    return "Contraseña incorrecta";
+                }
             }
         }
     }
